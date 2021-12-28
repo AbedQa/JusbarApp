a73358bcae4be7fb2c0b7cf82b675038c112c8ae
Abdelrahman Qasim <abdelrahman.qasim@orange.com>
2021-12-28 14:19:28 +0200

    Initial Commit

---
+++ JusbarApp/JusbarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JusbarAppApp.swift
+//  JusbarApp
+//
+//  Created by AbdelrahmanQasim on 12/28/21.
+//
+
+import SwiftUI
+
+@main
+struct JusbarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
